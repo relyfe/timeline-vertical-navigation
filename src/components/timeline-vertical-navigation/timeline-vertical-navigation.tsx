86646a93faff5319ee7290291a9bd2bb6f0cf34f
Matthieu Chavigny <matthieu.chavigny@relyfe.com>
2022-03-31 12:07:22 +0200

    moving current-date-underline

--- src/components/timeline-vertical-navigation/timeline-vertical-navigation.tsx
+++ src/components/timeline-vertical-navigation/timeline-vertical-navigation.tsx
@@ -6,12 +6,18 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class TimelineVerticalNavigation {
+  currentDateUnderlineElement!: HTMLElement;
+
+  onTvnInnerMouseMove = e => {
+    this.currentDateUnderlineElement.style.transform = `translateY(${e.offsetY}px)`;
+  };
+
   render() {
     return (
       <nav class="tvn-nav" role="slider" aria-valuemin="0" aria-valuemax="1" aria-valuenow="0" aria-orientation="vertical" aria-valuetext="">
-        <div class="tvn-inner">
+        <div class="tvn-inner" onMouseMove={e => this.onTvnInnerMouseMove(e)}>
           <div class="tvn-list"></div>
-          <div class="tvn-current-date-underline"></div>
+          <div class="tvn-current-date-underline" ref={el => (this.currentDateUnderlineElement = el)}></div>
           <div class="tvn-current-time-line"></div>
           <div class="tvn-current-date">
             <div class="tvn-current-date-label">mars 2015</div>
