84a7f80d2c8b586f2e2954c31d4f954f45e90b8a
Matthieu Chavigny <matttieu@gmail.com>
2022-04-24 11:04:34 +0200

    stencil config react build

--- stencil.config.ts
+++ stencil.config.ts
@@ -5,8 +5,9 @@ export const config: Config = {
   namespace: 'timeline-vertical-navigation',
   outputTargets: [
     reactOutputTarget({
-      componentCorePackage: 'react-timeline-vertical-navigation',
-      proxiesFile: '../react-timeline-vertical-navigation/src/index.ts',
+      componentCorePackage: 'timeline-vertical-navigation',
+      proxiesFile: '../react-library/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
     }),
     { type: 'docs-readme' },
     {
